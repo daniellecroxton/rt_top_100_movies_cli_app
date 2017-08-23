3ddf185a42d8c51a8aec1771b42077dff96bb6c1
daniellecroxton <danikrist@gmail.com>
2017-08-23 08:17:34 -0600

    Added and populated config/environment.rb

--- lib/rt_top_100_movies_cli_app.rb
+++ lib/rt_top_100_movies_cli_app.rb
@@ -1,12 +1,5 @@
-require 'open-uri'
-require 'nokogiri'
-require 'pry'
-
-require_relative "./rt_top_100_movies_cli_app/version"
-require_relative "./rt_top_100_movies_cli_app/movie"
-require_relative "./rt_top_100_movies_cli_app/scraper"
-require_relative "./rt_top_100_movies_cli_app/cli"
-
 module RtTop100MoviesCliApp
   # Your code goes here...
 end
+
+require_relative "../config/environment"
