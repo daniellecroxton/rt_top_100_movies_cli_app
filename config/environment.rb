0f5526d6ffadf882ed74a0318b70a4d15af4371c
daniellecroxton <danikrist@gmail.com>
2020-03-03 21:12:05 -0700

    adding untracked file

---
+++ config/environment.rb
@@ -0,0 +1,8 @@
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
+require_relative "../lib/rt_top_100_movies_cli_app/version"
+require_relative "../lib/rt_top_100_movies_cli_app/movie"
+require_relative "../lib/rt_top_100_movies_cli_app/scraper"
+require_relative "../lib/rt_top_100_movies_cli_app/cli"
